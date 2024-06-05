26427f97fda59daa65b9f0f70af68bd51d070c6f
Jona-St <js@papoo.de>
2024-06-05 15:03:15 +0200

    Description and version changes

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,19 +2,19 @@
 
 $EM_CONF['clickskeks'] = [
     'title' => 'Clickskeks',
-    'description' => 'Clickskeks (https://www.clickskeks.at/) Cookie-Banner with Data-Protection Plugin',
+    'description' => 'Clickskeks Cookie-Banner with Data-Protection Plugin. Requires a Clickskeks or an CCM19 subscription.',
     'category' => 'plugin',
     'author' => 'Jona Stephan',
     'author_email' => 'info@ccm19.de',
-    'company' => 'Papoo/&Media GmbH',
+    'author_company' => 'Papoo Software & Media GmbH',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.0.0',
+    'version' => '2.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0-0-12.4.99',
+            'typo3' => '12.0.0-13.1.99',
             'php' => '7.4.0-8.1.99'
         ],
         'conflicts' => [],
